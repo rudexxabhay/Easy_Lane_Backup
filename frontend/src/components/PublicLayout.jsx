c49d54adc681755c187ac98f6459ce9c74c6de3d
Karan Singh <ks8817112@gmail.com>
2026-07-25 12:53:38 +0530

    Initial commit

--- frontend/src/components/PublicLayout.jsx
+++ frontend/src/components/PublicLayout.jsx
@@ -2,5 +2,5 @@ import Navbar from './Navbar.jsx';
 import Footer from './Footer.jsx';
 
 export default function PublicLayout({ children }) {
-  return <div className="public-shell flex min-h-screen flex-col bg-white"><Navbar /><div className="public-content flex-1">{children}</div><Footer /></div>;
+  return <div className="flex min-h-screen flex-col bg-white"><Navbar /><div className="flex-1">{children}</div><Footer /></div>;
 }
